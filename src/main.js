5afb9a6b5147bcf21335723c487a4ed97422e5f7
Ryan <ryanchien8125@gmail.com>
2022-10-05 02:30:30 +0800

    安裝 vue3-google-login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import vue3GoogleLogin from 'vue3-google-login'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(vue3GoogleLogin, {
+  clientId: '這邊放上你的 Google Client ID'
+})
+
+app.mount('#app')
