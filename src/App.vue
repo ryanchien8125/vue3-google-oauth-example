2c08efa5de54f37dc69e9fee6ae0b5d4fd473fc9
Ryan <ryanchien8125@gmail.com>
2022-10-05 02:34:16 +0800

    使用 Google 渲染預設按鈕及 One Tap prompt

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const data = ref()
+
+const callback = (response) => {
+  data.value = response
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- 使用 Google 渲染預設按鈕 -->
+    <!-- <GoogleLogin :callback="callback" /> -->
+
+    <!-- 使用 Google 渲染預設按鈕及 One Tap prompt -->
+    <GoogleLogin :callback="callback" prompt />
+
+    <!-- 使用 Google 渲染預設按鈕及 One Tap prompt 並自動登入 -->
+    <!-- <GoogleLogin :callback="callback" prompt auto-login/> -->
+
+    <p>
+      {{ data }}
+    </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+p {
+  margin-top: 12px;
+  word-break: break-all;
 }
 </style>
