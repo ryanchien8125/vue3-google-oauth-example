8530e3da8202bec01ec01bf6506ae7acfb6ce4d7
Ryan <ryanchien8125@gmail.com>
2022-10-05 02:34:28 +0800

    使用 GoogleLogin 並在預設插槽內添加自定義按鈕

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,15 @@ const callback = (response) => {
     <!-- <GoogleLogin :callback="callback" /> -->
 
     <!-- 使用 Google 渲染預設按鈕及 One Tap prompt -->
-    <GoogleLogin :callback="callback" prompt />
+    <!-- <GoogleLogin :callback="callback" prompt /> -->
 
     <!-- 使用 Google 渲染預設按鈕及 One Tap prompt 並自動登入 -->
     <!-- <GoogleLogin :callback="callback" prompt auto-login/> -->
+
+    <!-- 使用 GoogleLogin 並在預設插槽內添加自定義按鈕 -->
+    <GoogleLogin :callback="callback">
+      <button>使用 Google 進行登入</button>
+    </GoogleLogin>
 
     <p>
       {{ data }}
